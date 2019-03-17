a35c43c43c5b20309f4fb5ee7dae5e7c9fcb46d7
umatbro <umatbroo@gmail.com>
2019-03-17 19:38:26 +0100

    change Movie title field to be unique (#12)

--- movies_api/models.py
+++ movies_api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Movie(models.Model):
-    title = models.CharField(max_length=120, blank=False)
+    title = models.CharField(max_length=120, blank=False, unique=True)
     cover = models.URLField(null=True)
     release_date = models.DateField(null=True)
     duration = models.IntegerField(null=True)  # movie duration in minutes
