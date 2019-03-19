1f55b308656bdff61045f9612599c8686a0cd19a
umatbro <umatbroo@gmail.com>
2019-03-19 22:40:29 +0100

    add comment filter by movie_id

--- comments/views.py
+++ comments/views.py
@@ -2,14 +2,17 @@ from typing import Any
 from rest_framework.generics import ListCreateAPIView
 from rest_framework.response import Response
 from rest_framework.request import Request
+from django_filters import rest_framework as dj_filters
 
-from comments import models, serializers
+from comments import models, serializers, filters
 import business_logic as bl
 
 
 class CommentView(ListCreateAPIView):
-    queryset = models.Comment
+    queryset = models.Comment.objects.all()
     serializer_class = serializers.CommentSerializer
+    filter_backends = (dj_filters.DjangoFilterBackend,)
+    filterset_class = filters.CommentFilter
 
     def post(self, request: Request, *args: Any, **kwargs: Any) -> Response:
         movie_id = request.data.get('movie_id', None)
